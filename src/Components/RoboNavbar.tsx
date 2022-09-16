e92096e4070e1342211b49a41be4c6c02500d25a
moriz <william.dedominico88@gmail.com>
2022-09-16 12:37:41 -0500

    Changed to dark

--- src/Components/RoboNavbar.tsx
+++ src/Components/RoboNavbar.tsx
@@ -6,9 +6,9 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 function RoboNavbar() {
     return (
-        <Navbar bg="light" expand="lg">
+        <Navbar bg="dark" expand="lg" sticky="top">
             <Container>
-                <Navbar.Brand href="/">
+                <Navbar.Brand href="/" style={{color:"white"}}>
                     <img
                         src="/logo192.png"
                         width="30"
@@ -21,35 +21,35 @@ function RoboNavbar() {
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
-                        <NavDropdown title="Teams" id="basic-nav-dropdown">
-                            <NavDropdown.Item href="/Teams/FRC">
+                        <NavDropdown title="Teams" id="basic-nav-dropdown" menuVariant="dark" style={{color:"white"}}>
+                            <NavDropdown.Item href="/Teams/FRC" style={{color:"white"}}>
                                 FRC
                             </NavDropdown.Item>
                             <NavDropdown.Divider/>
-                            <NavDropdown.Item href="/Teams/FTC">
+                            <NavDropdown.Item href="/Teams/FTC" style={{color:"white"}}>
                                 FTC
                             </NavDropdown.Item>
                         </NavDropdown>
-                        <Nav.Link href="/Volunteer">Volunteer</Nav.Link>
-                        <Nav.Link href="/Calender">Calender</Nav.Link>
-                        <NavDropdown title="Support Us" id="basic-nav-dropdown">
-                            <NavDropdown.Item href="/Support-Us/Booster">
+                        <Nav.Link href="/Volunteer" style={{color:"white"}}>Volunteer</Nav.Link>
+                        <Nav.Link href="/Calender" style={{color:"white"}}>Calender</Nav.Link>
+                        <NavDropdown title="Support Us" id="basic-nav-dropdown" menuVariant="dark">
+                            <NavDropdown.Item href="/Support-Us/Booster" style={{color:"white"}}>
                                 Booster
                             </NavDropdown.Item>
                             <NavDropdown.Divider/>
-                            <NavDropdown.Item href="/Support-Us/Store">
+                            <NavDropdown.Item href="/Support-Us/Store" style={{color:"white"}}>
                                 Store
                             </NavDropdown.Item>
                             <NavDropdown.Divider/>
-                            <NavDropdown.Item href="/Support-Us/Donate">
+                            <NavDropdown.Item href="/Support-Us/Donate" style={{color:"white"}}>
                                 Donate
                             </NavDropdown.Item>
                             <NavDropdown.Divider/>
-                            <NavDropdown.Item href="/Support-Us/Fundraising">
+                            <NavDropdown.Item href="/Support-Us/Fundraising" style={{color:"white"}}>
                                 Fundraising
                             </NavDropdown.Item>
                         </NavDropdown>
-                        <Nav.Link href="/Resources">Resources</Nav.Link>
+                        <Nav.Link href="/Resources" style={{color:"white"}}>Resources</Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
