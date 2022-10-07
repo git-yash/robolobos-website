1da81e8c06ea491625053018888ec082ec0534b2
moriz <william.dedominico88@gmail.com>
2022-10-07 11:58:28 -0500

    changed home page content

--- src/Screens/HomePage/HomePage.tsx
+++ src/Screens/HomePage/HomePage.tsx
@@ -35,7 +35,7 @@ const HomePage = () => {
 
                     <div style={{padding: 240}}></div>
 
-                    <h1 className="sponsorH1" style={{fontSize: 53}}>Sponsors</h1>
+                    {/*<h1 className="sponsorH1" style={{fontSize: 53}}>Sponsors</h1>
                     <div className="sponsorDiv">
                         <br/><br/><br/>
                         <div className="sponsorCardContainer">
@@ -48,26 +48,30 @@ const HomePage = () => {
                                 image="kalb.jpg"
                                 description="Sponsor of FTC roboticsSponsor of FTC roboticsSponsor of FTC roboticsSponsor of FTC robotics"/>
                         </div>
-                    </div>
+                    </div>*/}
 
-                    <div style={{padding: 100}}></div>
+                    {/*<div style={{padding: 100}}></div>*/}
 
                     <div className="aboutDiv">
-                        <h1 className="sponsorH1" style={{fontSize: 53, paddingBottom:0, height: 10}}>About</h1>
+                        <h1 className="sponsorH1" style={{fontSize: 53, paddingBottom:0, height: 10}}>The Teams</h1>
                         <div style={{padding: 50}}></div>
                         <h3 style={{textAlign: 'center', color: 'white', fontSize: 32, width: 1200, paddingLeft: 300, paddingTop: 0}}>
-                            Some very cool text about our brand and stuff Some very cool text about
-                            our brand and stuff Some very cool text about our brand and stuff Some
-                            very cool text about our brand and stuff Some very cool text about our
-                            brand and stuff Some very cool text about our brand and stuff Some very
-                            cool text about our brand and stuff Some very cool text about our brand
-                            and stuff Some very cool text about our brand and stuff Some very cool text
-                            about our brand and stuff Some very cool text about our brand and stuff So
+                            We have two teams FRC and FTC and some other text
                         </h3>
+                        <div className="centerContainer">
+                            <button className="btn btn-lg btn-outline-primary center">First Robotics Competition</button>
+                            <div style={{paddingLeft: 20, paddingRight: 20, display: "inline-block"}}/>
+                            <button className="btn btn-lg btn-outline-primary center">First Technology Competition</button>
+                        </div>
                     </div>
 
                     <div style={{padding: 100}}></div>
-                    <div style={{backgroundColor:"black"} }></div>
+
+                    <div className="imageCarousel">
+                        <h1>Image Carousel</h1>
+                    </div>
+
+                    <div style={{padding: 100}}></div>
                 </div>
             </body>
         </>
